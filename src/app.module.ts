20b836d7dfc83e09ab9cb5195d96b5064d911f09
lfhwnqe <lfhwnqea@gmail.com>
2025-05-25 09:49:15 +0800

    feat: 删除多余controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CommonModule } from './modules/common/common.module';
 import { UserModule } from './modules/user/user.module';
 import { TradeModule } from './modules/trade/trade.module';
@@ -9,8 +7,6 @@ import { ImageModule } from './modules/image/image.module';
 
 @Module({
   imports: [CommonModule, UserModule, TradeModule, ImageModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
